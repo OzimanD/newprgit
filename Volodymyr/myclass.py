b853fa4e7a1c13af6bce6510c6f70a5ac9559bfe
Volodymyr Malyi <vladimir28122007@gmail.com>
2026-06-12 19:19:22 +0200

    I wrote a comment for the 2nd task

--- Volodymyr/myclass.py
+++ Volodymyr/myclass.py
@@ -6,28 +6,45 @@
 надбавка дорівнює 2%; якщо стаж становить від 5 до 10 років — 5%. 
 Ввести з клавіатури зарплату та стаж, вивести надбавку та суму до виплати.
 '''
-def work_bonus(salary, work_experience):
+# def work_bonus(salary, work_experience):
+#
+#     if 2 <= work_experience <= 5:
+#         print("Your bonus is 2%")
+#         bonus_in_percent = 2
+#     elif 5 <= work_experience <= 10:
+#         print("Your bonus is 5%")
+#         bonus_in_percent = 5
+#     else:
+#         print("You dont have any bonus")
+#         bonus_in_percent = 0
+#
+#     bonus = salary * bonus_in_percent / 100 #добавил бы скобочки (salary) * (bonus_in_percent)
+#     full_salary = salary + bonus
+#     return bonus, full_salary
+#
+#
+# salary = float(input("Enter your salary: "))
+# work_experience = float(input("Enter your work experience in years: "))
+#
+# salary_result = work_bonus(salary, work_experience)
+#
+# print("Bonus: ", salary_result[0])
+# print("Total bonus: ", salary_result[1])
 
-    if 2 <= work_experience <= 5:
-        print("Your bonus is 2%")
-        bonus_in_percent = 2
-    elif 5 <= work_experience <= 10:
-        print("Your bonus is 5%")
-        bonus_in_percent = 5
-    else:
-        print("You dont have any bonus")
-        bonus_in_percent = 0
+'''
+1 Даны два целых числа A и B (A < B). Найти все целые числа, расположенные между данными числами (включая сами эти числа), в порядке их возрастания, а также количество N этих чисел.
+2Даны два целых числа A и B (A < B). Найти все целые числа, расположенные между данными числами (не включая сами эти числа), в порядке их убывания, а также количество N этих чисел.
+3Дано вещественное число A и целое число N (> 0). Найти A в степени N:
+AN = A·A··A (числа A перемножаются N раз) .
+'''
 
-    bonus = salary * bonus_in_percent / 100 #добавил бы скобочки (salary) * (bonus_in_percent)
-    full_salary = salary + bonus
-    return bonus, full_salary
-
-
-salary = float(input("Enter your salary: "))
-work_experience = float(input("Enter your work experience in years: "))
-
-salary_result = work_bonus(salary, work_experience)
+'''
+1 Даны два целых числа A и B (A < B). 
+Найти все целые числа, расположенные между данными числами (включая сами эти числа), в порядке их возрастания, 
+а также количество N этих чисел.
+'''
 
 
-print("Bonus: ", salary_result[0])
-print("Total bonus: ", salary_result[1])
+
+# def new_numbers(A, B):
+#     for number in range(A, B + 1):
