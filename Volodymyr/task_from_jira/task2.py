fcd979f4bf14f2b30d862fa4784719ef42a165c5
Yurii Ch <nbu.pki@gmail.com>
2026-06-16 19:28:34 +0200

    Code review for Task 2 - https://krasnyydmitriy-1781364439831.atlassian.net/browse/KAN-2

--- Volodymyr/task_from_jira/task2.py
+++ Volodymyr/task_from_jira/task2.py
@@ -9,6 +9,14 @@
 '''
 score_list = [45, 82, 12, 95, 67, 88]
 
-new_score_list = sorted(score_list, reverse=True)
+# new_score_list = sorted(score_list, reverse=True)
+# print(new_score_list[:3])
+# variant 2
+score_list.sort(reverse=True)
+print(score_list[:3])
+# variant 3
+for i in range(3):
+    print(max(score_list), end=' ')
+    score_list.remove(max(score_list))
 
-print(new_score_list[:3])
+
