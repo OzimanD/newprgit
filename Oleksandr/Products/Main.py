cbad52712ba94a87a4eeacabe736ae72ba9d9664
Oleksandr <olksandrmikhailovich85@gmail.com>
2026-07-11 19:40:27 +0200

    garet OOP

--- Oleksandr/Products/Main.py
+++ Oleksandr/Products/Main.py
@@ -2,5 +2,5 @@ from Objects import *
 from Methods import *
 
 print_product(products)
-write_file(products)
+writhe_file(products)
 read_file()
--- Oleksandr/Products/Methods.py
+++ Oleksandr/Products/Methods.py
@@ -4,7 +4,7 @@ from Objects import *
 def max_product(products):
     maxProduct = products[0]
     for product in products:
-        if products.get_quantity() > maxProduct.get_quantity():
+        if product.get_quantity() > maxProduct.get_quantity():
             maxProduct = product
     return maxProduct
 
@@ -15,7 +15,7 @@ def print_product(products):
 
 def writhe_file(products):
     file = open("result.txt","w", encoding="utf-8")
-    file.writhe(str(max_product(products)))
+    file.write(str(max_product(products)))
     file.close()
 
 def read_file():
